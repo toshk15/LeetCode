1e6f1c3ef888fe8a99ad29435348fa1d9bf40d6a
Fernando Rojas Ramos <45886706+toshk15@users.noreply.github.com>
2026-01-24 16:35:40 -0600

    Add Solution class for validating stack sequences
    Implement a solution to validate stack sequences.

---
+++ python/946. Validate Stack Sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack=[]
+        i=0
+        for n in pushed:
+            stack.append(n)
+            while i<len(popped) and stack and stack[-1]==popped[i]:
+                stack.pop()
+                i+=1
+        
+        return not stack
+        

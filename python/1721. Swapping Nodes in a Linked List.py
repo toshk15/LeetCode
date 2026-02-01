30cb6cf5654885866aa78f87668263f7b9139b1b
Fernando Rojas Ramos <45886706+toshk15@users.noreply.github.com>
2026-02-01 14:17:02 -0600

    Implement swapNodes function for linked list

---
+++ python/1721. Swapping Nodes in a Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        curr=head
+        while k-1>0:
+            curr=curr.next
+            k-=1
+        left=curr
+        right=head
+        while curr.next:
+            curr=curr.next
+            right=right.next
+        left.val,right.val=right.val,left.val
+        return head

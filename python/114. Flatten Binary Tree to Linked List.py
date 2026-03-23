ae52d44e8bf5a0d3a40c97037f58d615225c797c
Fernando Rojas Ramos <45886706+toshk15@users.noreply.github.com>
2026-03-23 12:44:56 -0600

    Add flatten method for binary tree to linked list
    Implement flatten method to convert binary tree to linked list in-place.

---
+++ python/114. Flatten Binary Tree to Linked List.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        stack=[root]
+        while stack:
+            curr = stack.pop()
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+            if stack:
+                curr.right=stack[-1]
+            curr.left=None
+        return curr

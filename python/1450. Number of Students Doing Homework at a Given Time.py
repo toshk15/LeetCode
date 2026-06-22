dfbafbf686c7ca5d228ec73e8360ca9cd53086c3
Fernando Rojas Ramos <45886706+toshk15@users.noreply.github.com>
2026-06-22 16:54:24 -0600

    Implement busyStudent method to count active students

---
+++ python/1450. Number of Students Doing Homework at a Given Time.py
@@ -0,0 +1,8 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        res=0
+        for i,j in zip(startTime,endTime):
+           if i<=queryTime<=j:
+              res+=1
+        return res
+           

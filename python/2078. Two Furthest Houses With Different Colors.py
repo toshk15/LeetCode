904a7033c3e7d4cd7706a2fd8bc7949c21f8bc3f
Fernando Rojas Ramos <45886706+toshk15@users.noreply.github.com>
2026-07-03 22:34:17 -0600

    Implement solution for furthest houses with different colors

---
+++ python/2078. Two Furthest Houses With Different Colors.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        n=len(colors)
+        max_dist=0
+        for i in range(n-1,-1,-1):
+            if colors[i]!=colors[0]:
+                max_dist=i
+                break
+        for i in range(n):
+            if colors[i]!=colors[-1]:
+                max_dist=max(max_dist,n-1-i)
+        return max_dist
+            

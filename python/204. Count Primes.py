46e6f4ab181c8b3f069aa4379643f0c23c413110
Fernando Rojas Ramos <45886706+toshk15@users.noreply.github.com>
2026-07-21 22:16:02 -0600

    Add Solution class to count prime numbers

---
+++ python/204. Count Primes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        primes=[True]*(n)
+        if n<=2:
+            return 0
+        primes[0]=False
+        primes[1]=False
+        for i in range(2,int(n**0.5)+1):
+            if primes[i]:
+                for j in range(i*i,n,i):
+                    primes[j]=False
+        return sum(primes)

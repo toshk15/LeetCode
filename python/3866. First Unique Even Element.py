441a8d9f826fcf34ef33dfd9314917fe5d6f1f1e
Fernando Rojas Ramos <45886706+toshk15@users.noreply.github.com>
2026-08-27 22:29:43 -0600

    Add solution for finding first unique even element

---
+++ python/3866. First Unique Even Element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstUniqueEven(self, nums: list[int]) -> int:
+        c=Counter(nums)
+        for n in nums:
+            if n%2==0 and c[n]==1:
+                return n
+        return -1
